a6c0d97db99044393b38c387b232cc2ee5b51591
lingarao yechuri <yechurilingarao25@gmail.com>
2021-01-24 20:51:42 -0500

    Database connection changes

--- app.js
+++ app.js
@@ -9,12 +9,26 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/todo', {useNewUrlParser: true, useUnifiedTopology: true});
 
 var path = require('path');
-
-const tasklist = mongoose.model('tasklist', {
+const taskschema =  new mongoose.Schema ({
   taskName: {
-   type: String,
+   type: String
   }
 });
+
+const tasklist = mongoose.model('tasklist', taskschema);
+
+const task1 = new tasklist({taskName: "sleeping"});
+const task2 = new tasklist({taskName: "eating"});
+const task3 = new tasklist({taskName: "coding"});
+
+const defaultTasks = [task1, task2, task3];
+
+const customList = mongoose.model('customList', {
+  listName: String,
+  tasks: [taskschema]
+});
+
+
 
 
 var work = [];
@@ -27,58 +41,100 @@ console.log(test);
 //var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 //var date = today.toLocaleDateString("en-US", options);
 
+app.get('/', (req, res) =>{
 
+});
 
-app.get('/todo', (req, res) => {
+app.get('/:customListName', (req, res) => {
   var task = [];
 //  let day = date1();
-  tasklist.find(function(err, tasks){
-    if(err){
-      console.log(err);
-    }else {
-      // tasks.forEach((item, i) => {
-      //   console.log(item.name);
-      //   console.log(i);
-      // });
-      console.log(tasks);
-
-      tasks.forEach((item)=>{
-          console.log("items"+item);
-          if(item.taskName != null){
-            console.log(item.taskName);
-            task.push(item.taskName);
-          }
-      });
-
-      res.render("index", {Today:test, newtask:task});
-    //  task.delete();
-    }
-  })
 
 
+  console.log("path"+req.params.customListName);
+  if(req.params.customListName == "todo"){
+    tasklist.find(function(err, tasks){
+      if(err){
+        console.log(err);
+      }else {
+        console.log(tasks);
+        res.render("index", {Today:"Today", newtask:tasks});
+      }
+    })
+  }else if (req.params.customListName == "work") {
+    res.render("index", {Today:"Work-List", newtask:work});
+  }else {
+    customList.findOne({listName: req.params.customListName}, function(err,foundlist) {
+       console.log("result"+foundlist);
+       if(foundlist == null){
+         const workList1 = new customList({listName: req.params.customListName, tasks: defaultTasks});
+         workList1.save();
+         res.redirect("/"+req.params.customListName);
+       }else {
+         console.log("already exists");
+         res.render("index", {Today:foundlist.listName, newtask:foundlist.tasks});
+       }
+    });
+
+  }
 });
 
 app.post('/', (req, res) => {
  console.log(req.body);
+ console.log("req.body.list"+req.body.list);
+
   if(req.body.list === "Work-List"){
     work.push(req.body.task);
     console.log(req.body.task);
     res.redirect("/work")
-  }else {
+  }else if(req.body.list == "Today") {
     //task.push(req.body.task);
     const newTask = new tasklist({taskName:req.body.task});
     newTask.save().then(() => console.log('saved'));
     console.log(req.body.task);
     res.redirect("/todo")
+
+  }else {
+    customList.findOne({listName: req.body.list}, function(err,foundlist) {
+      console.log("foundlist"+ foundlist);
+      const newtask = new tasklist({taskName: req.body.task});
+      foundlist.tasks.push(newtask);
+      foundlist.save();
+      console.log("foundlist"+foundlist);
+      res.redirect("/"+ req.body.list);
+    });
   }
+});
+
+app.post('/delete', (req, res) => {
+ console.log("indide delete test"+req.body.checkbox);
+ console.log("indide delete test"+req.body.listName);
+ if (req.body.listName == "Today") {
+     tasklist.deleteOne({ _id: req.body.checkbox }, function(err){
+          if(err){
+            console.log(err);
+          }else {
+
+              console.log("done!");
+              res.redirect("/todo")
+            }
+      });
+ }else {
+   customList.findOneAndUpdate({listName: req.body.listName}, {$pull: {tasks: {_id:req.body.checkbox }}}, function(err,foundlist) {
+      if (!err) {
+        res.redirect("/"+req.body.listName);
+      }
+
+   });
+
+ }
 
 });
 
-app.get('/work', (req, res) => {
-
-  res.render("index", {Today:"Work-List", newtask:work});
-
-});
+// app.get('/work', (req, res) => {
+//
+//   res.render("index", {Today:"Work-List", newtask:work});
+//
+// });
 
 
 app.listen(8085, function() {
